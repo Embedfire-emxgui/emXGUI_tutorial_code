2c4f08f15080b1ff7754208af521dbf869af9735
Wind <18575131670@163.com>
2019-03-18 15:02:52 +0800

    完成emXGUI文件系统的移植

---
+++ H743_Pro开发板/freeRTOS/05-emXGUI基础移植_触摸_资源设备_Fatfs/User/main.c
@@ -0,0 +1,253 @@
+/**
+  *********************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   FreeRTOS V9.0.0  + STM32 固件库例程
+  *********************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  **********************************************************************
+  */ 
+ 
+/*
+*************************************************************************
+*                             包含的头文件
+*************************************************************************
+*/ 
+/* FreeRTOS头文件 */
+#include "FreeRTOS.h"
+#include "task.h"
+/* 开发板硬件bsp头文件 */
+#include "board.h"
+
+#include <cm_backtrace.h>
+
+/* hardfault跟踪器需要的定义 */
+#define HARDWARE_VERSION               "V1.0.0"
+#define SOFTWARE_VERSION               "V0.1.0"
+
+/**************************** 任务句柄 ********************************/
+/* 
+ * 任务句柄是一个指针，用于指向一个任务，当任务创建好之后，它就具有了一个任务句柄
+ * 以后我们要想操作这个任务都需要通过这个任务句柄，如果是自身的任务操作自己，那么
+ * 这个句柄可以为NULL。
+ */
+
+/********************************** 内核对象句柄 *********************************/
+/*
+ * 信号量，消息队列，事件标志组，软件定时器这些都属于内核的对象，要想使用这些内核
+ * 对象，必须先创建，创建成功之后会返回一个相应的句柄。实际上就是一个指针，后续我
+ * 们就可以通过这个句柄操作这些内核对象。
+ *
+ * 内核对象说白了就是一种全局的数据结构，通过这些数据结构我们可以实现任务间的通信，
+ * 任务间的事件同步等各种功能。至于这些功能的实现我们是通过调用这些内核对象的函数
+ * 来完成的
+ * 
+ */
+
+
+/******************************* 全局变量声明 ************************************/
+/*
+ * 当我们在写应用程序的时候，可能需要用到一些全局变量。
+ */
+
+
+/*
+*************************************************************************
+*                             函数声明
+*************************************************************************
+*/
+static void GUI_Thread_Entry(void* pvParameters);/* Test_Task任务实现 */
+
+static void SystemClock_Config(void);
+static void BSP_Init(void);/* 用于初始化板载相关资源 */
+
+
+/***********************************************************************
+  * @ 函数名  ： BSP_Init
+  * @ 功能说明： 板级外设初始化，所有板子上的初始化均可放在这个函数里面
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  *********************************************************************/
+static void BSP_Init(void)
+{
+    /* Enable I-Cache */
+  SCB_EnableICache();
+
+  /* Enable D-Cache */
+  SCB_EnableDCache();
+
+  /* 系统时钟初始化成400MHz */
+	SystemClock_Config();
+  	
+  /*
+	 * STM32中断优先级分组为4，即4bit都用来表示抢占优先级，范围为：0~15
+	 * 优先级分组只需要分组一次即可，以后如果有其他的任务需要用到中断，
+	 * 都统一用这个优先级分组，千万不要再分组，切忌。
+	 */
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+  
+	/* 硬件BSP初始化统统放在这里，比如LED，串口，LCD等 */
+  SDRAM_Init();
+	/* LED 端口初始化 */
+	LED_GPIO_Config();	
+	
+	/* usart 端口初始化 */
+  UARTx_Config();
+  
+  /*hardfault 跟踪器初始化*/  
+  cm_backtrace_init("Fire_emxgui", HARDWARE_VERSION, SOFTWARE_VERSION);
+ 
+}
+
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 400000000 (CPU Clock)
+	*            HCLK(Hz)             = 200000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  100MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  100MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  100MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  100MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 160
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  HAL_StatusTypeDef ret = HAL_OK;
+  
+  /*使能供电配置更新 */
+  MODIFY_REG(PWR->CR3, PWR_CR3_SCUEN, 0);
+
+  /* 当器件的时钟频率低于最大系统频率时，电压调节可以优化功耗，
+		 关于系统频率的电压调节值的更新可以参考产品数据手册。  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+ 
+  /* 启用HSE振荡器并使用HSE作为源激活PLL */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 160;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+ 
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+  
+	/* 选择PLL作为系统时钟源并配置总线时钟分频器 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK  | \
+																 RCC_CLOCKTYPE_HCLK    | \
+																 RCC_CLOCKTYPE_D1PCLK1 | \
+																 RCC_CLOCKTYPE_PCLK1   | \
+                                 RCC_CLOCKTYPE_PCLK2   | \
+																 RCC_CLOCKTYPE_D3PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+}
+
+
+
+/*****************************************************************
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  * @note   第一步：开发板硬件初始化 
+            第二步：创建APP应用任务
+            第三步：启动FreeRTOS，开始多任务调度
+  ****************************************************************/
+int main(void)
+{	
+  BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+  
+  /* 开发板硬件初始化 */
+  BSP_Init();  
+  
+   /* 创建AppTaskCreate任务 */
+  xReturn = xTaskCreate((TaskFunction_t )GUI_Thread_Entry,  /* 任务入口函数 */
+                        (const char*    )"gui",/* 任务名字 */
+                        (uint16_t       )2*1024,  /* 任务栈大小 */
+                        (void*          )NULL,/* 任务入口函数参数 */
+                        (UBaseType_t    )3, /* 任务的优先级 */
+                        (TaskHandle_t*  )NULL);/* 任务控制块指针 */ 
+  /* 启动任务调度 */           
+  if(pdPASS == xReturn)
+    vTaskStartScheduler();   /* 启动任务，开启调度 */
+  else
+    return -1;  
+  
+  while(1);   /* 正常不会执行到这里 */    
+}
+
+
+extern void GUI_Startup(void);
+
+/**********************************************************************
+  * @ 函数名  ： gui_thread_entry
+  * @ 功能说明： gui_thread_entry任务主体
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void GUI_Thread_Entry(void* parameter)
+{	
+  
+  printf("野火emXGUI演示例程\n\n");
+
+  /* 执行本函数不会返回 */
+	GUI_Startup();
+  
+  while (1)
+  {
+    LED1_ON;
+    printf("Test_Task Running,LED1_ON\r\n");
+    vTaskDelay(500);   /* 延时500个tick */
+    
+    LED1_OFF;     
+    printf("Test_Task Running,LED1_OFF\r\n");
+    vTaskDelay(500);   /* 延时500个tick */
+  }
+}
+
+
+/********************************END OF FILE****************************/
