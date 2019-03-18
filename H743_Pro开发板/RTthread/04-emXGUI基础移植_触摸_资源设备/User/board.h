a37654040b884164690c966d2e585c5187828081
Wind <18575131670@163.com>
2019-03-18 15:17:30 +0800

    完成rtt--资源设备移植

---
+++ H743_Pro开发板/RTthread/04-emXGUI基础移植_触摸_资源设备/User/board.h
@@ -0,0 +1,14 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include "stm32h7xx.h"
+#include "./led/bsp_led.h"  
+#include "./usart/bsp_usart.h"
+#include "./touch/bsp_touch_gtxx.h"
+#include "./touch/bsp_i2c_touch.h"
+#include "./lcd/bsp_lcd.h"
+#include "./touch/palette.h"
+#endif 
+
+
+
