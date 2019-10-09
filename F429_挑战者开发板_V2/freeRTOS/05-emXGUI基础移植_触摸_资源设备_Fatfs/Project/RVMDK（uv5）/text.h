33972c9c371780c999dbe2a5feb3c52bb8eb8a16
Jc <798025823@qq.com>
2019-10-09 14:22:53 +0800

    复制V1工程 准备修改
    Former-commit-id: 5dabe6d33c3d7545d1db95c909a402deb3528990

---
+++ F429_挑战者开发板_V2/freeRTOS/05-emXGUI基础移植_触摸_资源设备_Fatfs/Project/RVMDK（uv5）/text.h
@@ -0,0 +1,9 @@
+#ifndef __TEXT_H__
+#define __TEXT_H__
+
+#include "emXGUI.h"
+
+void	Text_Create(void *param);
+static 	 LRESULT  	Text_proc2(HWND hwnd,UINT msg,WPARAM wParam,LPARAM lParam);
+
+#endif
